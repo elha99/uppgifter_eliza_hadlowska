d4b338c559abcf36a4aa220fbd8a0fdd1feda67d
Eliza <elha2612@student.miun.se>
2026-09-20 21:08:51 +0200

    Andra uppgiften är klar. Priset, antalet, summan utan moms och summan med moms skapades med varsin variabel. Skrivit ut koden med console.log för att se att det fungerar.

--- uppgift2.js
+++ uppgift2.js
@@ -1,2 +1,19 @@
 /* Lösning till Uppgift 2. Av Eliza Hadlowska, 2026*/
 "use strict";
+
+//Priset för produkten
+let price = 500;
+
+//Antal produkter
+let amount = 4;
+
+//Totala summan exklusive moms
+let summaUtanMoms = price * amount
+
+//Totala summan inklusive 25% moms
+let summaMedMoms = summaUtanMoms * 1.25
+
+console.log("Pris: " + price + " kr");
+console.log("Antal: " + amount);
+console.log("Totalt: " + summaUtanMoms + " kr");
+console.log("Totalt inklusive moms: " + summaMedMoms + " kr");
