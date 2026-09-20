d2e2225b98ae63fe7a553abe49b1a957f7678655
Eliza <elha2612@student.miun.se>
2026-09-20 20:48:54 +0200

    Första uppgiften är klar. Jag har lagt till förnamn, efternamn, ålder och boolean med information om student. Skrivit ut koden med console.log och kollat så den fungerar.

--- uppgift1.js
+++ uppgift1.js
@@ -1,2 +1,19 @@
 /* Lösning till Uppgift 1. Av Eliza Hadlowska, 2026*/
 "use strict";
+
+
+//Textsträng med förnamn, jag tänker const för att man brukar inte byta förnamn oftast
+const firstName = "Eliza";
+
+//Textsträng med efternamn, efternamnet kan bytas vid giftermål och annat så det fungerar bättre med let
+let lastName = "Hadlowska";
+
+//Ålder
+let age = 27;
+
+//En boolean som anger om personen är student
+let isStudent = true;
+
+console.log(firstName + " " + lastName);
+console.log("Ålder: " + age);
+console.log("Student: " + isStudent);
