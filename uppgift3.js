28ced9c8a230952c4664df27fb74aa7eb28ff46b
Eliza <elha2612@student.miun.se>
2026-09-21 20:53:31 +0200

    Tredje uppgiften är klar. Variabeln med åldern är kollad så den skriver ut rätt ord beroende på ålder med hjälp av if, else if och else

--- uppgift3.js
+++ uppgift3.js
@@ -1,2 +1,14 @@
 /* Lösning till Uppgift 3. Av Eliza Hadlowska, 2026*/
 "use strict";
+
+//Variabel med ålder
+let age = 35
+
+//Meddelande beroende på ålder
+if (age < 18) {
+    console.log("Barn")
+} else if (age >= 18 && age < 65) {
+    console.log("Vuxen")
+} else {
+    console.log("Pensionär")
+}
