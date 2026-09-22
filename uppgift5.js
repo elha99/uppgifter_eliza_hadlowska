a6f6972e8d9979cb4a27cd874e395d350fe704d8
Eliza <elha2612@student.miun.se>
2026-09-22 20:36:46 +0200

    Femte uppgiften är klar. Gjort en array med fem maträtter, skrivit ut hela, bara första och endast sista elementet. Lagt till och tagit bort från arrayen och skrivit ut ändringar.

--- uppgift5.js
+++ uppgift5.js
@@ -1,2 +1,19 @@
 /* Lösning till Uppgift 5. Av Eliza Hadlowska, 2026*/
 "use strict";
+
+//Array med maträtter
+let matMeny = ["Pizza", "Kebab", "Korvgryta", "Pasta Carbonara", "Tacos"];
+
+//Skriva ut hela arrayen
+console.log(matMeny);
+//Skriva ut första elementet
+console.log(matMeny[0]);
+//Skriva ut sista elementet
+console.log(matMeny[4]);
+
+//Lägga till Tomatsoppa sist i arrayen
+matMeny.push("Tomatsoppa");
+//Ta bort den första maträtten i arrayen
+matMeny.shift();
+//Skriva ut hela arrayen
+console.log(matMeny)
