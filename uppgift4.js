b961cb06fcabea50d1612f7e157a4e76d482c1b6
Eliza <elha2612@student.miun.se>
2026-09-22 20:11:29 +0200

    Fjärde uppgiften är klar. For-loop med startvärde, villkor och steg, samt ett villkor inuti loopen, för att skriva ut endast de jämna talen.

--- uppgift4.js
+++ uppgift4.js
@@ -1,2 +1,10 @@
 /* Lösning till Uppgift 4. Av Eliza Hadlowska, 2026*/
 "use strict";
+
+//startvärde 1, talet ska vara mindre eller lika med 20, och öka med 1
+for (let i = 1; i <= 20; i++) { 
+//Om talet är delbart med 2 så blir det ingen rest över, === är att både värdet och datatypen är lika, alltså 0, då ska den skriva ut den siffran
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
