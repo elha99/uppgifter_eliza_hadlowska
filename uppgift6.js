5f0f1a6c8d2eba950b60354961018d428a712e45
Eliza <elha2612@student.miun.se>
2026-09-23 18:55:01 +0200

    Sjätte uppgiften klar. Jag har gjort en funktion som tar in parametrarna bredd och höjd. Dessa ska multipliceras och returnera summan. Testat att skriva ut tre värden för att se så det fungerar.

--- uppgift6.js
+++ uppgift6.js
@@ -1,2 +1,15 @@
 /* Lösning till Uppgift 6. Av Eliza Hadlowska, 2026*/
 "use strict";
+
+//Funktion som räknar ut arean av en rektangel
+function calculateArea(width, height) {
+    //Arean = bredden * höjden
+    let sum = width * height;
+    //sum = arean
+    return sum;
+}
+
+//Olika värden för att testa att det fungerar, skriver ut värdena med texten "Arean är"
+console.log("Arean är " + calculateArea(4, 10));
+console.log("Arean är " + calculateArea(18, 24));
+console.log("Arean är " + calculateArea(25, 35));
