fc0b81edaaecfcefb714a9179d997b265adc6df2
Eliza <elha2612@student.miun.se>
2026-09-23 19:52:01 +0200

    Åttonde uppgiften är klar. Jag har skapat ett objekt (book1) som innehåller titel, författare och utgivningsår. Därefter har jag skapat en funktion som skriver ut informationen på ett tydligt sätt. och tillsist anropa funktionen så den syns i konsollen.

--- uppgift8.js
+++ uppgift8.js
@@ -1,2 +1,20 @@
 /* Lösning till Uppgift 8. Av Eliza Hadlowska, 2026*/
 "use strict";
+
+//Objekt som består av en bok, inuti objektet är det titel, författare och utgivningsår
+const book1 = {
+    title: "Hunger Games",
+    writer: "Suzanne Collins",
+    publicationYear: 2011
+};
+
+//Funktion om bokinformation där den tar emot objektet ovanför som parameter
+function bookInformation(book1) {
+    //Funktionen skriver ut information om boken på tydligt sätt 
+    console.log("Titel: " + book1.title)
+    console.log("Författare: " + book1.writer)
+    console.log("Utgivningsår: " + book1.publicationYear)
+}
+
+//Anropa funktionen
+bookInformation(book1)
